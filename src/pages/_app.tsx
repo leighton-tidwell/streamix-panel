3f90b2180477155f58bf1ff49909115b90048e09
Leighton Tidwell <leighton.tidwell57@gmail.com>
2023-08-31 20:08:57 -0500

    poppins font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
@@ -8,7 +16,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>Page title</title>
+        <title>Streamix Panel</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
@@ -21,6 +29,7 @@ export default function App(props: AppProps) {
         theme={{
           /** Put your mantine theme override here */
           colorScheme: "dark",
+          fontFamily: poppins.style.fontFamily,
         }}
       >
         <Component {...pageProps} />
