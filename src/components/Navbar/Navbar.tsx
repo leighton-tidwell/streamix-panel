3a68e35c9d0e6bf2d6ff5dd8a0aed8a29094a768
Leighton Tidwell <leighton.tidwell57@gmail.com>
2023-08-31 20:08:41 -0500

    nav links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,31 @@
-import { Navbar as MantineNavbar } from "@mantine/core";
+import { Navbar as MantineNavbar, NavLink, NavLinkProps } from "@mantine/core";
+
+const StyledNavLink = (props: NavLinkProps) => {
+  return (
+    <NavLink
+      {...props}
+      bg="hsl(225, 7%, 10%)"
+      sx={{
+        "&:hover": {
+          color: "white",
+        },
+      }}
+    />
+  );
+};
 
 export const Navbar = () => {
   return (
-    <MantineNavbar width={{ base: 300 }} p="xs">
-      Navbar
+    <MantineNavbar
+      width={{ base: 300 }}
+      p="xs"
+      sx={{ gap: ".5em" }}
+      withBorder={false}
+    >
+      <StyledNavLink label="Stream Dashboard" />
+      <StyledNavLink label="Relays" />
+      <StyledNavLink label="Recordings" />
+      <StyledNavLink label="Access" />
     </MantineNavbar>
   );
 };
