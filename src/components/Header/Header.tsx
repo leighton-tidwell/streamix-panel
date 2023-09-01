8b98e6961e9174d952e6bf5f77188aedd057470a
Leighton Tidwell <leighton.tidwell57@gmail.com>
2023-08-31 20:08:48 -0500

    modify header style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,10 +7,17 @@ export const Header = () => {
       height={50}
       p="xs"
       display="flex"
-      sx={{ gap: ".5em", alignItems: "center", img: { borderRadius: "5px" } }}
+      sx={{
+        gap: ".5em",
+        alignItems: "center",
+        img: { borderRadius: "5px" },
+        borderBottom: "5px solid hsl(225, 7%, 10%)",
+      }}
     >
       <Image src="/logo.png" width={30} height={30} alt="Streamix home" />
-      <Text color="white">Streamix Panel</Text>
+      <Text color="white" weight="bold" size="xl">
+        Streamix Panel
+      </Text>
     </MantineHeader>
   );
 };
